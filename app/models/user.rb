2fcab08ee1071d5694690ad4019ac8df40ef91da
mo-rin <i.get.sunny@gmail.com>
2020-09-15 19:38:04 +0900

    userモデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,11 @@ class User < ApplicationRecord
 
   with_options presence: true do
   validates :nickname
-  validates :email, uniqueness: true, inclusion: { in: "@" }
   validates :password, length: { minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください'}
   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-  validates :first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-  validates :last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
   validates :birthday
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {"aaaa@gmail.com"}
+    password              {"aom13680"}
     password_confirmation {password}
+    first_name            {"たかユキ"}
+    last_name             {"阿部"}
+    first_name_kana       {"タカユキ"}
+    last_name_kana        {"アベ"}
+    birthday              {"1930-01-01"}
   end
 end
