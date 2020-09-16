30aad18382672d02bbdf1140aebcb2b438c4e80e
mo-rin <i.get.sunny@gmail.com>
2020-09-16 11:54:43 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   with_options presence: true do
     validates :nickname
     validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください'}
---
+++ db/migrate/20200915144450_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string  :item_name, null: false
+      t.text  :text, null: false
+      t.integer  :price, null: false
+      t.integer :category_id, foreign_key: true
+      t.integer :status_id, foreign_key: true
+      t.integer :delivery_fee_id, foreign_key: true
+      t.integer :option_id, foreign_key: true
+      t.integer :shipping_days_id, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
