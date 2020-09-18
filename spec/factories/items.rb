bf9aba869514370c23fcdabf0ac64443feca3884
mo-rin <i.get.sunny@gmail.com>
2020-09-18 19:05:07 +0900

    itemモデルの単体テストのexampleの整理

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name        {"負け犬"}
+    text             {"自分自身に負けた者"}
+    category         {"メンズ"}
+    status           {"未使用に近い"}
+    delivery_fee     {"着払い（購入者負担）"}
+    option           {"鳥取県"}
+    shipping_day     {"2〜3日で発送"}
+    price            {"90000"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規商品出品登録' do
+    context '新規登録がうまくいくとき' do
+      it "item_nameとtext、 categoryとstatus、 delivery_feeとoption、shipping_dayとpriceがあれば登録できる" do
+
+      end
+      it "priceが¥300~¥9,999,999の間であれば登録できる" do
+      end
+      it "priceが半角数字であれば登録できる" do
+      end
+    end
+
+    context '新規商品出品登録がうまくいかないとき' do
+      it "item_nameが空だと登録できない" do
+      end
+      it "textが空では登録できない" do
+      end
+      
+      it "statusが空では登録できない" do
+      end
+      it "delivery_feeが空では登録できない" do
+      end
+      it "optionが空では登録できない" do
+      end
+      it "shipping_dayが空では登録できない" do
+      end
+      it "priceが空では登録できない" do
+      end
+      it "priceが¥300~¥9,999,999の範囲外であれば登録できない" do
+      end
+      it "priceが全角数字では登録できない" do
+      end
+    end
+  end
 end
