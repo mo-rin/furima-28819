87f20b41d71f8ff3a0af36e118a1f35631542cb2
mo-rin <i.get.sunny@gmail.com>
2020-09-21 19:56:50 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,16 +2,17 @@ FactoryBot.define do
   factory :item do
     item_name        {"負け犬"}
     text             {"自分自身に負けた者"}
-    category         {"メンズ"}
-    status           {"未使用に近い"}
-    delivery_fee     {"着払い（購入者負担）"}
-    option           {"鳥取県"}
-    shipping_day     {"2〜3日で発送"}
+    category_id         { 2 }
+    status_id           { 2 }
+    delivery_fee_id     { 2 }
+    option_id           { 2 }
+    shipping_day_id     { 2 }
     price            {"90000"}
     association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
  
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
-    email                 { 'Faker::Internet.free_email' }
-    password = Faker::Internet.password(min_length: 8)
+    email                 { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation { password }
-    first_name            { Faker::Name.first_name }
-    last_name             { Faker::Name.last_name }
+    first_name            { "孝之" }
+    last_name             { "阿部" }
     first_name_kana       { 'タカユキ' }
     last_name_kana        { 'アベ' }
     birthday              { '1930-01-01' }
