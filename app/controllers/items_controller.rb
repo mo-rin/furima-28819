2787f001d40fb005e0be8bfe4d4ac8146b641230
mo-rin <i.get.sunny@gmail.com>
2020-09-23 21:16:39 +0900

    レビュー修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,24 +18,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
 
   def show
     @item = Item.find(params[:id])
